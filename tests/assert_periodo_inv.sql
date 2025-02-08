51a12839adf896cb21b7cf7694e921220d5daaeb
cauaholanda777@gmail.com <cauaholanda777@gmail.com>
2025-02-08 23:12:56 +0000

    formatando test

--- tests/assert_periodo_inv.sql
+++ tests/assert_periodo_inv.sql
@@ -1,16 +1,12 @@
-with test_periodo_inv
-as
-(
-    select cast(max(kafi_dh_ocorrreal) as date) as max_periodo
-    from {{ source('prevencao-perdas', 'kardex_perdas') }}
-    where kafi_tp_mov in('EA', 'SA', 'E9', 'S9')
-    and kafi_tx_nr_docto like '%/%'
-),
-diff_date as
-(
-    select date_diff('day', max_periodo, current_date) as diff from test_periodo_inv
-)
+with
+    test_periodo_inv as (
+        select cast(max(kafi_dh_ocorrreal) as date) as max_periodo
+        from {{ source("prevencao-perdas", "kardex_perdas") }}
+        where kafi_tp_mov in ('EA', 'SA', 'E9', 'S9') and kafi_tx_nr_docto like '%/%'
+    ),
+    diff_date as (
+        select date_diff('day', max_periodo, current_date) as diff from test_periodo_inv
+    )
 
-select
-   case when diff >= 2 then false else true end as test
+select case when diff >= 2 then false else true end as test
 from diff_date
